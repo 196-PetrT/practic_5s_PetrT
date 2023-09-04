32bcd58aee9eac8348d9cec42eee55bb80052c5d
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-05 00:40:50 +0300

    dif max min

---
+++ Task38/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+
+double[] Array = new double[5]; // по другому не смог
+
+void GenerateArray()// метод создает ранд массив вещественных чисел
+{
+    
+    Random rand = new Random();
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = rand.NextDouble();
+        Console.Write($"{Array[i]:F2} "); // обрезаю до второй запятой и вывожу
+    }
+
+}
+
+
+double DifMaxMin(double[] Array) // метод считает разницу между max и min
+{
+    double max = Array[0];
+    double min = Array[0];
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > max) max = Array[i];
+
+        else if (Array[i] < min) min = Array[i];
+    }
+    return max - min;
+}
+
+GenerateArray();
+Console.Write($"Разница между максимальным и минимальным элементов массива: {DifMaxMin(Array):F2}");
